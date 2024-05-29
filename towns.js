51d737195275f910f4d9168dc003cbb73d266fce
manuel <manuelnikolov05@gmail.com>
2024-05-29 21:02:30 +0300

    Improved styling and effects for the functionality to add a new town

--- towns.js
+++ towns.js
@@ -27,19 +27,25 @@ function shuffleArray(array) {
 		} 
 	} 
 
-function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}	
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
-	
+function deleteTown() { 
+	let townName = $('#townName').val(); 
+	$('#townName').val(''); 
+	let removed = false; 
+	for (let option of $('#towns option')) { 
+		if (option.textContent == townName) { 
+			removed = true; 
+			option.remove(); 
+		} 
+	} 
+	if (removed) 
+		showMessage(townName + " deleted."); 
+	else 
+		showMessage(townName + " not found."); 
+	} 
+
+function showMessage(msg) { 
+	$('#result').text(msg).css("display", "block"); 
+	setTimeout(function () { 
+		$('#result').hide('blind', {}, 500); 
+	}, 3000); 
 }
